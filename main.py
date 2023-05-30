be72a44c71172df06d35a45223362b3f410d4e29
Kyuwoong Lee <kwooler@gmail.com>
2023-05-29 17:20:16 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+##################################################
+# Comlete your code here
+##################################################
+
+# numbers = list(map(int, input().split()))
+
+def main():
+    main.numbers = list(map(int, input().split()))
+    #  Use main.ithmin to assign your result
+
+    ith = int(input())
+
+    # Code your program here
+
+    print('The ith min val: ', main.ithmin)
+##
+
+
+if __name__ == '__main__':
+    main()
---
+++ main_test.py
@@ -0,0 +1,50 @@
+import main
+import io
+import sys
+import re
+
+
+def test_main_1():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = '1 2 3 4 5\n2'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    # regex_string = r'[\w,\W]*1'
+    # regex_string += r'[\w,\W]*3'
+    # regex_string += r'[\w,\W]*5'
+    # regex_string += r'[\w,\W]*'
+    # print(regex_string)
+    # res = re.search(regex_string, main.evenlist)
+    # assert res != None
+    # print(res.group())
+    assert main.main.ithmin == 2
+
+
+def test_main_2():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = '5 25 15 10 0\n3'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    # regex_string = r'[\w,\W]*1'
+    # regex_string += r'[\w,\W]*3'
+    # regex_string += r'[\w,\W]*5'
+    # regex_string += r'[\w,\W]*'
+    # print(regex_string)
+    # res = re.search(regex_string, main.evenlist)
+    # assert res != None
+    # print(res.group())
+    assert main.main.ithmin == 10
