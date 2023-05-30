f289f35659b00fc23a0b7b22c3e0c8a62dc4b07d
Kyu Lee <kwooler@gmail.com>
2023-05-29 18:59:28 -0700

    L710

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
-##################################################
-# Comlete your code here
-##################################################
 
-# numbers = list(map(int, input().split()))
+def findSmallest(numbers, i):
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+
+
+def selectionSort(numbers):
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+
 
 def main():
-    main.numbers = list(map(int, input().split()))
-    #  Use main.ithmin to assign your result
+    # numbers = list(map(int, input().split()))
+    numbers = [25, 15, 5, 10, 0]
+    findSmallest(numbers, 0)
+    print(f'Find the 1st smallest number: {numbers}')
 
-    ith = int(input())
+    findSmallest(numbers, 1)
+    print(f'Find the 2nd smallest number: {numbers}')
 
+    findSmallest(numbers, 2)
+    print(f'Find the 3rd smallest number: {numbers}')
+
+    findSmallest(numbers, 3)
+    print(f'Find the 4th smallest number: {numbers}')
+
+    findSmallest(numbers, 4)
+    print(f'Find the 5th smallest number: {numbers}')
     # Code your program here
 
-    print('The ith min val: ', main.ithmin)
-##
+    numbers = [25, 15, 5, 10, 0]
+    selectionSort(numbers)
+    print(f'After call selectionSort(): {numbers}')
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -7,15 +7,30 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '1 2 3 4 5\n2'
-    sys.stdin = io.StringIO(datastr)
+    # datastr = '1 2 3 4 5\n2'
+    # sys.stdin = io.StringIO(datastr)
 
-    main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
+    numbers = [25, 15, 5, 10, 0]
+    main.findSmallest(numbers, 0)
+    print(f'Find the 1st smallest number: {numbers}')
+    assert numbers[0] == 0
+    main.findSmallest(numbers, 1)
+    print(f'Find the 2nd smallest number: {numbers}')
+    assert numbers[1] == 5
+    main.findSmallest(numbers, 2)
+    print(f'Find the 3rd smallest number: {numbers}')
+    assert numbers[2] == 10
+    main.findSmallest(numbers, 3)
+    print(f'Find the 4th smallest number: {numbers}')
+    assert numbers[3] == 15
+    main.findSmallest(numbers, 4)
+    print(f'Find the 5th smallest number: {numbers}')
+    assert numbers[4] == 25
     # regex_string = r'[\w,\W]*1'
     # regex_string += r'[\w,\W]*3'
     # regex_string += r'[\w,\W]*5'
@@ -24,21 +39,23 @@ def test_main_1():
     # res = re.search(regex_string, main.evenlist)
     # assert res != None
     # print(res.group())
-    assert main.main.ithmin == 2
 
 
 def test_main_2():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '5 25 15 10 0\n3'
-    sys.stdin = io.StringIO(datastr)
+    # datastr = '5 25 15 10 0\n3'
+    # sys.stdin = io.StringIO(datastr)
 
-    main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
+    numbers = [25, 15, 5, 10, 0]
+    main.selectionSort(numbers)
+    print(f'After call selectionSort(): {numbers}')
+    assert numbers == [0, 5, 10, 15, 25]
     # regex_string = r'[\w,\W]*1'
     # regex_string += r'[\w,\W]*3'
     # regex_string += r'[\w,\W]*5'
@@ -47,4 +64,3 @@ def test_main_2():
     # res = re.search(regex_string, main.evenlist)
     # assert res != None
     # print(res.group())
-    assert main.main.ithmin == 10
